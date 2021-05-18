afba1d68ef293875b752d93db6f9d339817f7bce
TomVu <tom.vu@nexusfrontier.tech>
2021-05-18 18:18:39 +0700

    Add material ui

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import { ThemeProvider } from '@material-ui/core/styles';
 import ReduxPage from './pages/ReduxPage';
+
+const theme = {};
 
 function App() {
   return (
     <div className="App">
-      This is admid page
-      <ReduxPage />
+      <ThemeProvider theme={theme}>
+        This is admid page
+        <ReduxPage />
+      </ThemeProvider>
     </div>
   );
 }
